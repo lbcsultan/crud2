0c69265d75489de773e370f4a93ad97657b34e3e
Byoungcheon Lee <lbcsultan@gmail.com>
2023-12-04 17:24:55 +0900

    2nd

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
 import TopicList from '@/components/TopicList'
+import { getServerSession } from 'next-auth'
 import Image from 'next/image'
+import { authOptions } from './api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  console.log(session)
+
+  if (!session) {
+    redirect('/signIn')
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold">Recent News</h1>
